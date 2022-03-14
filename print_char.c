0f5b593fe957b9392e1a4b91541a1fdc8abc4897
Cienna Nguyen <4326@holbertonschool.com>
2022-03-15 09:44:31 +1100

    Add code to print_char.c (still incorrect)

--- print_char.c
+++ print_char.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include "main.h"
 
 /**
   * print_char - function to print char %c
-  * @format: %c
+  * @format: copy of format char pointed to %c
   * Return: if char is NULL, return pointer
   */
 char *print_char(char *format, va_list *var)
 {
+	char *formatEnd;
+	char *c;
+	int i = 0; /*placeholder of c */
+
+	c = va_arg(*var, char *);
+	if((c == NULL) || (*c == '\0') || (*(c + 1) != '\0'))
+		return (format);
+
+	formatEnd = malloc(sizeof(*formatEnd) * (_strlen(format) + 1));
+	if (formatEnd == NULL)
+		return (format);
+	_strcpy(formatEnd, format);
+	_strcpy(format, c);
+
+	format = format + _strlen(c);
+	if (_strlen(formatEnd) > 2)
+		_strcpy(format, formatEnd + 2);
+
+	free(formatEnd);
 	return (format);
 }
